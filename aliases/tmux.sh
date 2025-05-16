c06d372d5222678a76b0684af81a0fbab166f87e
lpt <patrickli0917@gmail.com>
2025-05-17 02:50:31 +0800

    fix bug

--- aliases/tmux.sh
+++ aliases/tmux.sh
@@ -2,10 +2,12 @@ if [[ -n "$TMUX" ]]; then
   echo -e '\e[5 q'
 fi
 
+# auto start tmux
 if which tmux 2>&1 >/dev/null; then
-  if ls /tmp/tmux-* >/dev/null 2>&1;then
-    if [ $TERM != "screen-256color" ] && [  $TERM != "screen" ]; then
-      tmux attach -t default || tmux new -s default
-    fi
+  if [ $TERM != "screen-256color" ] && [  $TERM != "screen" ] && [ -e ~/.tmux-auto-start ]; then
+    tmux attach -t default || tmux new -s default
   fi
 fi
+
+alias t-en="touch ~/.tmux-auto-start"
+alias t-dis="rm -f ~/.tmux-auto-start"
