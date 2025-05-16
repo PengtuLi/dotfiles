3c0286702c7a82c75103447632fa25ecb76d85d5
lpt <patrickli0917@gmail.com>
2025-05-17 01:46:05 +0800

    fix bug

--- aliases/tmux.sh
+++ aliases/tmux.sh
@@ -3,7 +3,9 @@ if [[ -n "$TMUX" ]]; then
 fi
 
 if which tmux 2>&1 >/dev/null; then
-  if [ $TERM != "screen-256color" ] && [  $TERM != "screen" ]; then
-    tmux attach -t default || tmux new -s default; exit
+  if ls /tmp/tmux-* >/dev/null 2>&1;then
+    if [ $TERM != "screen-256color" ] && [  $TERM != "screen" ]; then
+      tmux attach -t default || tmux new -s default
+    fi
   fi
 fi
