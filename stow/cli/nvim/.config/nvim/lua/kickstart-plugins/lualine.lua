6de6814b2ca7934512101fd808657e1ddcd62f33
Orion Li <373595331@qq.com>
2025-12-08 09:15:09 +0000

    feat: rm tab lualine

--- stow/cli/nvim/.config/nvim/lua/kickstart-plugins/lualine.lua
+++ stow/cli/nvim/.config/nvim/lua/kickstart-plugins/lualine.lua
@@ -37,25 +37,40 @@ return {
         lualine_c = {
           {
             'filename',
+            path = 1, -- 使用相对路径
+            file_status = false, -- 这里不显示状态符号
+            fmt = function(str)
+              -- 正则表达式：提取最后一个 "/" 之前的所有内容
+              local dir = str:match '(.*/)'
+              return dir or '' -- 如果没有目录（在根目录），则返回空
+            end,
+            padding = { left = 1, right = 0 },
+            color = { fg = 'White', gui = 'bold,italic' }, -- 路径颜色 (Dracula 紫色或灰色)
+            shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+          },
+          {
+            'filename',
             file_status = true, -- Displays file status (readonly status, modified status)
             newfile_status = true, -- Display new file status (new file means no write after created)
-            path = 1,
+            path = 0,
             -- 0: Just the filename
             -- 1: Relative path
             -- 2: Absolute path
             -- 3: Absolute path, with tilde as the home directory
             -- 4: Filename and parent dir, with tilde as the home directory
 
-            shorting_target = 40, -- Shortens path to leave 40 spaces in the window
-            -- for other components. (terrible name, any suggestions?)
             symbols = {
-              modified = '[-]', -- Text to show when the file is modified.
-              readonly = '[x]', -- Text to show when the file is non-modifiable or readonly.
+              modified = '[●]', -- Text to show when the file is modified.
+              readonly = '[]', -- Text to show when the file is non-modifiable or readonly.
               unnamed = '[?]', -- Text to show for unnamed buffers.
               newfile = '[+]', -- Text to show for newly created file before first write
             },
+            color = {
+              fg = 'White',
+              bg = 'Green',
+              gui = 'bold', -- 字体样式 (加粗)
+            },
           },
-          ts_breadcrumb,
         },
         lualine_x = {
           'searchcount',
@@ -86,7 +101,7 @@ return {
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = { 'filename', ts_breadcrumb },
+        lualine_c = { 'filename' },
         lualine_x = {},
         lualine_y = {},
         lualine_z = { 'progress', '%l:%LLOC' },
@@ -94,7 +109,7 @@ return {
       tabline = {},
       extensions = {},
       winbar = {
-        lualine_a = { 'buffers' },
+        -- lualine_a = { 'buffers' },
       },
       inactive_winbar = {
         -- lualine_a = { 'buffers' },
