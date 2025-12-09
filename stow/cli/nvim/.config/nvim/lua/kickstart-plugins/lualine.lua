ddaa5b7c8a706c9650b681a0fab8e44fd552a848
Orion Li <373595331@qq.com>
2025-12-09 14:31:38 +0800

    feat: unify color to tokyonight

--- stow/cli/nvim/.config/nvim/lua/kickstart-plugins/lualine.lua
+++ stow/cli/nvim/.config/nvim/lua/kickstart-plugins/lualine.lua
@@ -14,7 +14,7 @@ return {
     require('lualine').setup {
       options = {
         icons_enabled = true,
-        theme = 'dracula',
+        theme = 'tokyonight',
         component_separators = { left = '', right = '' },
         section_separators = { left = '', right = '' },
         disabled_filetypes = {
